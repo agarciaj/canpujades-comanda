68d927531fd0431d1baa9c9d98b061dc6b354baf
agarciaj <angela.g.jerez@gmail.com>
2016-02-28 19:59:20 +0100

    Added files via upload
    Primera pujada dels arxius de l'aplicació de comandes de Can Pujades

---
+++ comanda/docs/index_es.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Help</title>
+<style>
+	div#stageWrap {width:80%; margin:10px auto;}
+	body {font-family:Arial;}
+	.faq-item {margin-bottom:20px;}
+</style>
+</head>
+<body>
+<div id="stageWrap">
+	<h1>Preguntas Frecuentes(FAQ)</h1>
+	<a name="top"></a>
+	<ul>
+		<li><a href="#makeOrder">¿Cómo hago mi pedido?</a></li>
+		<li><a href="#printOrder">¿Cómo imprimo mi pedido?</a></li>
+	</ul>
+
+<div class="faq-item">
+	<h2>¿Cómo hago mi pedido?</h2>
+	<a name="makeOrder"></a>
+	<ol>
+		<li>Hacer clic en "Siguente pedido"</li>
+		<li>Seleccionar "Por proveedor" o "Por categoría de productos". Aparecerán los productos habilitados para la fecha indicada</li>
+		<li>Completar las cantidades de cada producto y al hacer clic fuera o tabulador, la cantidad queda ingresada</li>
+		<li>Finalmente en el panel de resumen (a la derecha), hacer clic en "Guardar"</li>
+	<ol>
+</div>
+<a href="#top">Back to top</a>
+
+<div class="faq-item">
+	<h2>¿Cómo imprimo mi pedido?</h2>
+	<a name="printOrder"></a>
+	<p>Para obtener una versión imprimible del pedido, basta hacer lo siguiente: </p>
+        <ol>
+                <li>Hacer clic en "Mi(s) pedido(s)"</li>
+                <li>En la parte superior derecha de los pedidos, donde termina la línea del título, aparecerá un ícono de impresora. Al hacer clic en él, obtendrá una versión imprimible de su(s) pedido(s)</li>
+        <ol>
+</div>
+<a href="#top">Back to top</a>
+
+
+</div>
+
+</body>
+</html>
---
+++ comanda/js/aixadacart/i18n/cart.locale-ca-va.js
@@ -0,0 +1,41 @@
+(function($){
+
+	$.extend({
+		aixadacart : {
+			title : {
+				shop 	: "La cistella",
+				order	: "La comanda",
+				preorder : "Acumulativa"
+			},
+			validate : "Valida",
+			submit	: "Envia",
+			save 	: "Desa",
+			saving	: "Guardant...",
+			searchAdd : "Cerca un altre producte",
+			total : "Total",
+			total_net : "Sense impostos",
+			subtotal : "Subtotal",
+			ivaTax : "IVA",
+			ivaTaxAbbrev : "IVA",
+			ivaTaxInclAbbrev : "incl. IVA",		
+			revTax	: "Impost revolucionari",
+			revTaxAbbrev: "ImpRev",
+			revTaxInclAbbrev: "incl. ImpRev",
+			tblCol : {
+				productName : "Nom",
+				providerName : "Proveïdor",
+				quantity : "Quantitat",
+				quantityAbbrev : "Qu",
+				unit : "Unitat",
+			},
+			msg : {
+				saveSuccess : "S'han desat correctement els productes!",
+				errInput	: "La cantidad ha de ser numeric!",
+				orderClosed : "Aquet producto forma part de una comanda ya tancada y no es pot modificar mes."
+				
+			}
+		
+		}
+		
+	});
+})( jQuery );
